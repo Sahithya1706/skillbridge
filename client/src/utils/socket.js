20c71532b3820334d25946b55c372514dc92ed14
Sahithya1708 <sahithyahegde2917@gmail.com>
2026-02-13 13:02:31 +0530

    fix: socket.io production URL

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://skillbridge-backend-hz7v.onrender.com", {
   transports: ["websocket"],
 });
 
